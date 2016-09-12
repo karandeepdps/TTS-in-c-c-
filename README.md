35984f742e2a1f60759f4e3b13034144c8af4027
Karandeep <karandeepdps@gmail.com>
2016-09-12 17:09:12 +0530

    Fixed exit bug when string is more than 100 char

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # TTS-in-c-c-
+Text to Speech convertor in c/c++.
+For linux,osx,windows.
+Current Release only supports:Osx.
+
+
+Usage:
+Compile tts.c++ using g++
+Then run the compiled file.
+Follow on screen instructions.
+
+
+In next release:
+->Support for windows,linux.
+->Read from file.
+->Bug fixes.
+
+
+(c) karandeepdps@gmail.com
--- tts.cpp
+++ tts.cpp
@@ -34,17 +34,18 @@ int playSound2( char *filename )
 
 int main()
 {
-	char word[100];
-	char mesg[100];
+	char word[100000];
+	char mesg[100000];
 
 	while(1)
 	{
-		memset(mesg,0,100*sizeof(char));
-		memset(word,0,100*sizeof(char));
+		memset(mesg,0,100000*sizeof(char));
+		memset(word,0,100000*sizeof(char));
 		cout<<"Enter String"<<endl;
 		fgets(mesg,1024,stdin);
 
 
+		int counter=0;
 
         			
         					int kd=0,jj=0;
@@ -54,6 +55,16 @@ int main()
         						if(mesg[kd]==' ')
         							word[jj]='+';
         						kd++;jj++;
+        						counter++;
+        						if(counter>80 &&  )
+        						{
+
+        						playSound2(word);
+        						counter=0;
+        						memset(word,0,100000*sizeof(char));
+        						jj=0;
+
+        						}
         					}
         				//	printf("google string%s\n",word);
         					playSound2(word);
